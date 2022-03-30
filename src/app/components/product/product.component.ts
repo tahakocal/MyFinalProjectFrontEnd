406450e0da8f76d064a4a2f43430ecebe3e66c6d
tahakocal <tahakocalgs@gmail.com>
2022-03-30 19:43:12 +0300

    initial commit

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  product1 = {
+    productId: 1,
+    productName: 'Bardak',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product2 = {
+    productId: 2,
+    productName: 'Laptop',
+    categoryId: 1,
+    unitPrice: 15,
+  };
+  product3 = {
+    productId: 3,
+    productName: 'Mouse',
+    categoryId: 1,
+    unitPrice: 7,
+  };
+  product4 = {
+    productId: 4,
+    productName: 'Keyboard',
+    categoryId: 1,
+    unitPrice: 7,
+  };
+  product5 = {
+    productId: 5,
+    productName: 'Camera',
+    categoryId: 1,
+    unitPrice: 6,
+  };
+
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5,
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
